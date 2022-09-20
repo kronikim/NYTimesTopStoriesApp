522d369fcbcc68b5e5a9d84a9807287406c39afc
deniz karahan <denizkarahan88@gmail.com>
2022-09-20 23:04:22 +0200

    Initial Commit

---
+++ NYTimesTopStoriesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYTimesTopStoriesApp
+//
+//  Created by deniz karahan on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

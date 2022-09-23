b7429039269d1062996803febfcb4f7004f1d75d
deniz karahan <denizkarahan88@gmail.com>
2022-09-23 13:59:21 +0200

    Network Response carry to new file from Network

--- NYTimesTopStoriesApp/Network/Network.swift
+++ NYTimesTopStoriesApp/Network/Network.swift
@@ -93,20 +93,4 @@ private extension Network {
         default: return .failed
         }
     }
-        
-    enum NetworkResponse:String {
-        case decodeDataProblem = "Decode Data Problem"
-        case authenticationError = "You need to be authenticated first."
-        case badRequest = "Bad request"
-        case outdated = "The url you requested is outdated."
-        case failed = "Network request failed."
-        case noData = "Response returned with no data to decode."
-        case unableToDecode = "We could not decode the response."
-        case noInternet = "Please check your network connection."
-        case tooManyRequest = "Too Many Request, Quota limit exceeded."
-        case uriTooLong = "URI too long"
-        case payloadTooLarge = "Payload Too Large."
-        case requestTimeOut = "Request Timeout"
-        case requestConflict = "Request Conflict"
-    }
 }
